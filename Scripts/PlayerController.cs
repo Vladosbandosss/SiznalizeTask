7e3fcf5f0513e28b7daca6ad83125d1e128e7538
Vladosbandosss <dasistperfektos@gmail.com>
2021-08-24 18:57:22 +0300

    MusicTask

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int playerSpeed;
+
+    private Rigidbody2D _rb;
+    private float inputX;
+    private float move;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+     
+        if (Input.GetKey(KeyCode.D))
+        {
+            float inputX = Input.GetAxis("Horizontal");
+            float move = playerSpeed * inputX;
+           _rb.velocity = new Vector3(move, 0);
+        }
+
+       else if (Input.GetKey(KeyCode.A))
+        {
+             inputX = Input.GetAxis("Horizontal");
+             float move = playerSpeed * inputX;
+            _rb.velocity = new Vector3(move, 0);
+        }
+    }
+}
---
+++ Scripts/ThiefCheck.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThiefCheck : MonoBehaviour
+{
+     AudioSource myfx;
+    Coroutine musicPaly;
+    private float _minVolume = 1f;
+    private float _changeVolumeSize = 0.2f;
+ 
+    private void Start()
+    {
+        myfx = GetComponent<AudioSource>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+           musicPaly =  StartCoroutine(CallingRing());
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            Debug.Log("игрок вышел");
+            StopCoroutine(musicPaly);
+        }
+    }
+    IEnumerator CallingRing()
+    {
+        float voll = 0f;
+        for (int i = 0; i < int.MaxValue; i++)
+        {
+            myfx.volume = voll;
+            myfx.Play();
+            
+            yield return new WaitForSeconds(1f);
+
+            if (voll >= _minVolume)
+            {
+                voll = 0f;
+            }
+
+            if (voll< _minVolume)
+            {
+                voll += _changeVolumeSize;
+            }
+            
+        }
+
+    }
+
+}
